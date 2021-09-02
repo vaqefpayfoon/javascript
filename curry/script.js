fdc0457d5ad2f3fe73b4bd227ae642b98f5d40d5
vaqef.payfoon@outlook.com <vaqef.payfoon@outlook.com>
2021-09-03 02:07:11 +0430

    node js and node type script

--- curry/script.js
+++ curry/script.js
@@ -1,16 +1,2 @@
-import { curry } from 'lodash/fp'
-function sum(a) {
-    return b => {
-        return a + b
-    }
-}
-
-console.log(sum(2)(3));
-
-function sumBind(a, b) {
-    return a + b;
-}
-
-// console.log(sumBind.bind(null, 2)(3));
-
-console.log(curry(sumBind(1)(2)))
+const _ = require('lodash');
+console.log(_.VERSION)
